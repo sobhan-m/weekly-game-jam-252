b719590c2aeb9a185b9db0b421fff7a3e19a38ab
Sobhan <s.kevishahi@gmail.com>
2022-06-02 14:04:02 -0400

    Implemented Weapon Swapping

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float damage = 10f;
+    [SerializeField] Sprite ketchupSprite;
+    [SerializeField] Sprite mayonnaiseSprite;
+    [SerializeField] Sprite mustardSprite;
+
+    private WeaponTypes bulletType = WeaponTypes.KETCHUP;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,5 +23,35 @@ public class Bullet : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void ChangeBulletSprite(WeaponTypes type)
+    {
+        switch (type)
+        {
+            case WeaponTypes.KETCHUP:
+                GetComponent<SpriteRenderer>().sprite = ketchupSprite;
+                break;
+            case WeaponTypes.MAYONNAISE:
+                GetComponent<SpriteRenderer>().sprite = mayonnaiseSprite;
+                break;
+            case WeaponTypes.MUSTARD:
+                GetComponent<SpriteRenderer>().sprite = mustardSprite;
+                break;
+            default:
+                Debug.Log("Debug Bullet.SetBulletType(): Type does not match cases.");
+                break;
+        }
+    }
+
+    public void SetBulletType(WeaponTypes type)
+    {
+        bulletType = type;
+        ChangeBulletSprite(type);
+    }
+
+    public WeaponTypes GetBulletType()
+    {
+        return bulletType;
     }
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,12 +7,14 @@ public class Gun : MonoBehaviour
     [SerializeField] Transform bulletSource;
     [SerializeField] GameObject bulletPrefab;
 
+    private WeaponTypes gunType = WeaponTypes.KETCHUP;
+
     public float shotSpeed = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -22,12 +24,39 @@ public class Gun : MonoBehaviour
         {
             Shoot();
         }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gunType = WeaponTypes.KETCHUP;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gunType = WeaponTypes.MAYONNAISE;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            gunType = WeaponTypes.MUSTARD;
+        }
     }
 
     void Shoot()
     {
+        // Creating correct weapon type.
         GameObject shot = Instantiate(bulletPrefab, bulletSource.position, bulletSource.rotation);
+        shot.GetComponent<Bullet>().SetBulletType(gunType);
+
+        // Bullet movement.
         Rigidbody2D rb = shot.GetComponent<Rigidbody2D>();
         rb.AddForce(bulletSource.up * shotSpeed, ForceMode2D.Impulse);
+    }
+
+    public void SetGunType(WeaponTypes type)
+    {
+        gunType = type;
+    }
+
+    public WeaponTypes GetGunType()
+    {
+        return gunType;
     }
 }
