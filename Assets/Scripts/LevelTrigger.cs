abcc28b2e5441126b42f58c3900b7a92010b399f
Sobhan <s.kevishahi@gmail.com>
2022-08-08 14:59:49 -0400

    Created Level Trigger

---
+++ Assets/Scripts/LevelTrigger.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTrigger : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() == null)
+        {
+            Debug.Log("LevelTrigger.OnTriggerEnter2D(): Nonplayer entered.");
+            return;
+        }
+
+        if (levelManager == null)
+        {
+            Debug.Log("LevelTrigger.OnTriggerEnter2D(): No level manager found.");
+            return;
+        }
+
+        levelManager.LoadNextLevel();
+    }
+}
